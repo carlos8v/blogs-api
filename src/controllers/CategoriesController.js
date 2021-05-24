d9ba4e436393898913ab248ff04ebf83a24298d5
Carlos Souza <carlos.pessoal@hotmail.com>
2021-05-24 15:16:40 -0300

    Sexto requisito

--- src/controllers/CategoriesController.js
+++ src/controllers/CategoriesController.js
@@ -1,13 +1,19 @@
 const route = require('express').Router();
 
 const { Category } = require('../models');
-const { CREATED } = require('./status');
+const { CREATED, SUCCESS } = require('./status');
 
 const authMiddleware = require('../middlewares/authMiddleware');
 const categoryMiddleware = require('../middlewares/categoryMiddleware');
 
-route.use(categoryMiddleware);
 route.use(authMiddleware);
+
+route.get('/', async (req, res) => {
+  const categories = await Category.findAll();  
+  return res.status(SUCCESS).json(categories);
+});
+
+route.use(categoryMiddleware);
 
 route.post('/', async (req, res) => {
   const { name } = req.body;
